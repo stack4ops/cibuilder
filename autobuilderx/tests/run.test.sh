8a41dcce49ef5cccb9d33f3448674e5d2cfa753a
Stefan Schneider <eqsoft4@gmail.com>
2025-11-18 08:25:19 +0100

    add pinned images for testing

--- autobuilderx/tests/run.test.sh
+++ autobuilderx/tests/run.test.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-log 2 "test disabled"
+ret=$(docker run --rm $docker_run_options --name $test_container_name $test_image docker buildx version | grep "github.com/docker/buildx")
 
-# ret=$(docker run --rm $docker_run_options --name $test_container_name $test_image docker buildx version | grep "github.com/docker/buildx")
+if [ "$?" != "0" ] || [ -z "$ret" ] ; then
+  log 3 "Test failed!"
+  exit 1
+fi
 
-# if [ "$?" != "0" ] || [ -z "$ret" ] ; then
-#   log 3 "Test failed!"
-#   exit 1
-# fi
-
-# log 5 "detected buildx version in dockerx image: $ret"
+log 5 "detected buildx version in dockerx image: $ret"
