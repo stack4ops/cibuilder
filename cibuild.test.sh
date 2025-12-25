baeb5ff04240c4b547f24500d49592987289b762
Stefan Schneider <eqsoft4@gmail.com>
2025-12-25 22:38:16 +0100

    edit test script

--- cibuild.test.sh
+++ cibuild.test.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-# if ! docker run --rm --name $container $test_image docker buildx version | grep "github.com/docker/buildx"; then
-#   log 0 "[failed] Test failed!"
-#   exit 1
-# fi
-
-log 1 "[warn] Test not implemented"
+assert_log "github.com/docker/buildx" "/bin/sh" "-c" "docker buildx version; sleep infinity"
