423ee8a0a129464c9ecf110e1bf7d3c7aa5f96d5
Stefan Schneider <eqsoft4@gmail.com>
2026-02-24 16:10:42 +0100

    edit entrypoint for github sources

--- cibuild_entrypoint.sh
+++ cibuild_entrypoint.sh
@@ -8,12 +8,12 @@ export DOCKER_CONFIG="${DOCKER_CONFIG:-/home/user/.docker}"
 # only dynamic cibuild loading libs if not locked
 if [ ! -d "/tmp/cibuilder.locked" ]; then
     if [ -n "${CIBUILDER_BIN_URL:-}" ] || [ -n "${CIBUILDER_BIN_REF:-}" ]; then
-        CIBUILDER_BIN_URL="${CIBUILDER_BIN_URL:-https://gitlab.com/stack4ops/public/cibuild/-/archive}"
+        CIBUILDER_BIN_URL="${CIBUILDER_BIN_URL:-https://github.com/stack4ops/cibuild/archive/refs/heads}"
         CIBUILDER_BIN_REF="${CIBUILDER_BIN_REF:-main}"
 
         echo "using CIBUILDER_BIN_URL: ${CIBUILDER_BIN_URL}"
         echo "using CIBUILDER_BIN_REF: ${CIBUILDER_BIN_REF}"
-        
+
         cd /home/user
 
         if [ -d "bin" ]; then
@@ -21,7 +21,9 @@ if [ ! -d "/tmp/cibuilder.locked" ]; then
             rm -r "bin"
         fi
 
-        curl -L -s "${CIBUILDER_BIN_URL}/${CIBUILDER_BIN_REF}/cibuild-${CIBUILDER_BIN_REF}.tar.gz" | tar xzf - --strip-components=1 "cibuild-${CIBUILDER_BIN_REF}/bin"
+        curl -L -s "${CIBUILDER_BIN_URL}/${CIBUILDER_BIN_REF}.tar.gz" \
+        | tar xzf - --strip-components=2 "cibuild-${CIBUILDER_BIN_REF}/bin"
+
         chmod -R 755 "bin"
     fi
 fi
